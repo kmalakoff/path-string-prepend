c591597621d3f781155b428b23a98869b8ff0f4f
Kevin Malakoff <kmalakoff@gmail.com>
2024-11-29 22:51:12 -0800

    Migrate to ESM and Biome

--- test/spec/changes.test.js
+++ test/spec/changes.test.js
@@ -1,35 +1,37 @@
-var assert = require('assert');
+const assert = require('assert');
 
-var prepend = require('../..');
-var DELIMITER = process.platform === 'win32' ? ';' : ':';
+const prepend = require('path-string-prepend');
 
-describe('changes', function () {
-  it('prepends - exists at front', function () {
-    var envPaths = ['install/path', 'other/path', 'another/path'];
-    var envPath = envPaths.join(DELIMITER);
+const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
+const DELIMITER = isWindows ? ';' : ':';
 
-    var changes = prepend(envPath, 'install/path', { changes: true });
+describe('changes', () => {
+  it('prepends - exists at front', () => {
+    const envPaths = ['install/path', 'other/path', 'another/path'];
+    const envPath = envPaths.join(DELIMITER);
+
+    const changes = prepend(envPath, 'install/path', { changes: true });
     assert.equal(changes.added.length, 0);
     assert.equal(changes.removed.length, 0);
     assert.equal(changes.path, envPath);
   });
 
-  it('prepends - removes extra', function () {
-    var envPaths = ['install/path', 'other/path', 'another/path', 'install/path', 'other/path', 'another/path'];
-    var envPath = envPaths.join(DELIMITER);
+  it('prepends - removes extra', () => {
+    const envPaths = ['install/path', 'other/path', 'another/path', 'install/path', 'other/path', 'another/path'];
+    const envPath = envPaths.join(DELIMITER);
 
-    var changes = prepend(envPath, 'install/path', { changes: true });
+    const changes = prepend(envPath, 'install/path', { changes: true });
     assert.equal(changes.added.length, 0);
     assert.equal(changes.removed.length, 1);
     envPaths.splice(3, 1);
     assert.equal(changes.path, envPaths.join(DELIMITER));
   });
 
-  it('prepends - removes extras', function () {
-    var envPaths = ['install/path', 'other/path', 'another/path', 'install/path', 'other/path', 'another/path', 'install/path'];
-    var envPath = envPaths.join(DELIMITER);
+  it('prepends - removes extras', () => {
+    const envPaths = ['install/path', 'other/path', 'another/path', 'install/path', 'other/path', 'another/path', 'install/path'];
+    const envPath = envPaths.join(DELIMITER);
 
-    var changes = prepend(envPath, 'install/path', { changes: true });
+    const changes = prepend(envPath, 'install/path', { changes: true });
     assert.equal(changes.added.length, 0);
     assert.equal(changes.removed.length, 2);
     envPaths.splice(6, 1);
@@ -37,24 +39,24 @@ describe('changes', function () {
     assert.equal(changes.path, envPaths.join(DELIMITER));
   });
 
-  it('adds missing no entries', function () {
-    var envPaths = ['other/path', 'another/path', 'other/path', 'another/path'];
-    var envPath = envPaths.join(DELIMITER);
+  it('adds missing no entries', () => {
+    const envPaths = ['other/path', 'another/path', 'other/path', 'another/path'];
+    const envPath = envPaths.join(DELIMITER);
 
-    var changes = prepend(envPath, 'install/path', { changes: true });
+    const changes = prepend(envPath, 'install/path', { changes: true });
     assert.equal(changes.added.length, 1);
     assert.equal(changes.removed.length, 0);
-    assert.equal(changes.path, 'install/path' + DELIMITER + envPaths.join(DELIMITER));
+    assert.equal(changes.path, `install/path${DELIMITER}${envPaths.join(DELIMITER)}`);
   });
 
-  it('adds missing and removes middle', function () {
-    var envPaths = ['other/path', 'another/path', 'install/path', 'other/path', 'another/path'];
-    var envPath = envPaths.join(DELIMITER);
+  it('adds missing and removes middle', () => {
+    const envPaths = ['other/path', 'another/path', 'install/path', 'other/path', 'another/path'];
+    const envPath = envPaths.join(DELIMITER);
 
-    var changes = prepend(envPath, 'install/path', { changes: true });
+    const changes = prepend(envPath, 'install/path', { changes: true });
     assert.equal(changes.added.length, 1);
     assert.equal(changes.removed.length, 1);
     envPaths.splice(2, 1);
-    assert.equal(changes.path, 'install/path' + DELIMITER + envPaths.join(DELIMITER));
+    assert.equal(changes.path, `install/path${DELIMITER}${envPaths.join(DELIMITER)}`);
   });
 });
