ca890e69287697a51f4b21a20a812bba5443faf3
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-02 06:08:50 -0800

    Upgrade modules

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -45,4 +45,4 @@ function prepend(pathString, prependPath, options) {
     changes.path = parts.join(delimiter);
     return options.changes ? changes : changes.path;
 }
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
