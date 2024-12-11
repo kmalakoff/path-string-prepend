faa317c6baa32bcb20acb5996b8f52a313b692ee
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-10 18:09:29 -0800

    Upgrade modules

--- dist/cjs/polyfills.js
+++ dist/cjs/polyfills.js
@@ -4,4 +4,4 @@ if (!path.delimiter) {
     var isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
     path.delimiter = isWindows ? ';' : ':';
 }
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
